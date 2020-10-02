e6dc5e36f63ea112a86d411399dd612638add7c3
Ryusuke <ps34h.k.r@gmail.com>
2020-10-02 17:11:01 +0900

    関数をクラスで実装し，tkinterテンプレート作成

--- python/gui/guidata.py
+++ python/gui/guidata.py
@@ -1,32 +1,47 @@
-from tkinter import *
+# -*- coding: utf-8 -*-
+
+import tkinter as tk
 from tkinter import ttk
-
-# ルートウィンドウ要素の作成
-root = Tk()
-
-# ウィンドウタイトルの設定
-root.title('ウィンドウタイトル')
-
-frame1 = ttk.Frame(root, padding=16)
-label1 = ttk.Label(frame1, text='Your name')
-t = StringVar()
-entry1 = ttk.Entry(frame1, textvariable=t)
-button1 = ttk.Button(
-    frame1,
-    text='OK',
-    command=lambda: print('Hello, %s.' % t.get())
-)
-
-frame2 = ttk.Frame(root, padding=16)
-label2 = ttk.Label(frame1, text='aaa')
-
-# レイアウト
-frame1.pack()
-label1.pack(side=LEFT)
-entry1.pack(side=LEFT)
-button1.pack(side=LEFT)
-frame2.pack()
-label2.pack(side=LEFT)
+from ttkthemes import *
 
 
-root.mainloop()
+class Application(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+
+        frame1 = ttk.Frame(master, padding=16)
+        label1 = ttk.Label(frame1, text='Your name')
+        t = tk.StringVar()
+        entry1 = ttk.Entry(frame1, textvariable=t)
+        button1 = ttk.Button(
+            frame1,
+            text='OK',
+            command=lambda: print('Hello, %s.' % t.get())
+        )
+
+        frame2 = ttk.Frame(master, padding=16)
+        label2 = ttk.Label(frame2, text='aaa')
+        t2 = tk.StringVar()
+        entry2 = ttk.Entry(frame2, textvariable=t2)
+
+        # レイアウト
+        frame1.pack()
+        label1.grid(row=0, column=0)
+        entry1.grid(row=0, column=1)
+        button1.grid(row=1, column=1)
+        frame2.pack()
+        label2.grid(row=2, column=0)
+        entry2.grid(row=2, column=1)
+
+
+if __name__ == '__main__':
+    # ルートウィンドウ要素の作成
+    root = tk.Tk()
+
+    # ウィンドウタイトルの設定
+    root.title('ウィンドウタイトル')
+
+    # クラス呼び出し
+    Application(master=root)
+
+    root.mainloop()
